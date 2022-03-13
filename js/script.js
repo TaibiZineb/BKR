541d640e504da4b486ea0cb0d1a2157eb9668594
unknown <taibizineb96@gmail.com>
2022-03-13 20:44:19 +0100

    changement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var burgerMenu = document.querySelector(".hamburger");
-var Nav = document.querySelector(".navbar-nav");
+var Nav = document.querySelector(".nav-m");
 
 
 burgerMenu.addEventListener("click", () => {
@@ -7,7 +7,7 @@ burgerMenu.addEventListener("click", () => {
     Nav.classList.toggle("active");
 
 })
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => 
+document.querySelectorAll(".nav-m").forEach(n => n.addEventListener("click", () => 
 {
     burgerMenu.classList.remove("active");
     Nav.classList.remove("active");
