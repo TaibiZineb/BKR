f60eb0f42e55c246b9cdb50c59514cc8e150e1d4
unknown <taibizineb96@gmail.com>
2022-03-15 10:10:58 +0100

    slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 var burgerMenu = document.querySelector(".hamburger");
 var Nav = document.querySelector(".nav-m");
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
     Nav.classList.toggle("active");
@@ -17,26 +14,4 @@ document.querySelectorAll(".nav-m").forEach(n => n.addEventListener("click", () 
 }))
 
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("custom-slider");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
