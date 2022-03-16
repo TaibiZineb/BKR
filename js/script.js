31bf8a6f97bf60a2bdbd7f4947f13420738a2a76
unknown <taibizineb96@gmail.com>
2022-03-16 10:33:21 +0100

    Slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var burgerMenu = document.querySelector(".hamburger");
 var Nav = document.querySelector(".nav-m");
+var visibleDiv = 0;
 
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
@@ -12,8 +13,38 @@ document.querySelectorAll(".nav-m").forEach(n => n.addEventListener("click", () 
     Nav.classList.remove("active");
 
 }))
-
-
+    function showDiv()
+    {
+      $(".grid").hide();
+      $(".grid:eq("+ visibleDiv +")").show();
+    }
+    showDiv()
+  
+  function showNext()
+  {
+    if(visibleDiv== $(".grid").length-1)
+    {
+      visibleDiv = 0;
+    }
+    else {
+      visibleDiv ++;
+    }
+    showDiv();
+  }
+  
+  
+  function showPrev()
+  {
+    if (visibleDiv == 0)
+    {
+      visibleDiv= $(".grid").length-1
+    }
+    else {
+      visibleDiv --;
+    }
+    showDiv();
+  }
+  
 
 
 
